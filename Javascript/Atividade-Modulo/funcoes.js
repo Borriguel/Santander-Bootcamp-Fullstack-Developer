82a81a6053f2229d65c49e828ae7aa295981041a
Borriguel <rodolpho-henrique@hotmail.com.br>
2022-06-24 03:48:38 -0300

    Atividade modulos

---
+++ Javascript/Atividade-Modulo/funcoes.js
@@ -0,0 +1,17 @@
+function mostraIdade (nome, idade) {
+    return `A idade de ${nome} é ${idade}`;
+}
+
+function mostraCidade (cidade) {
+    return `Mora em ${cidade}`;
+}
+
+function mostraHobby (hobby) {
+    return `Gosta de ${hobby}`;
+}
+
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ Javascript/Atividade-Modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade('Rodolpho', 26));
+console.log(mostraCidade('Mesquita'));
+console.log(mostraHobby('Codding'));
